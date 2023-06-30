2831d64710c38d7d02d3e06a23bc1a31ed680925
Deepu Mohan Puthrote <git@deepumohan.com>
2023-06-30 21:39:40 +0100

    Add schema for RSS subscription

--- src/com/eelchat/schema.clj
+++ src/com/eelchat/schema.clj
@@ -25,6 +25,16 @@
              [:chan/title :string]
              [:chan/comm :comm/id]]
 
+   :sub/id       :uuid
+   :subscription [:map {:closed true}
+                  [:xt/id             :sub/id]
+                  [:sub/url           :string]
+                  [:sub/chan          :chan/id]
+                  [:sub/last-post-uri {:optional true} :string]
+                  [:sub/fetched-at    {:optional true} inst?]
+                  [:sub/last-modified {:optional true} :string]
+                  [:sub/etag          {:optional true} :string]]
+
    :msg/id :uuid
    :message [:map {:closed true}
              [:xt/id :msg/id]
